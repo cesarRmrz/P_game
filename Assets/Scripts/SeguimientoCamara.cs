c452bba2c81c3be5a2a060e818af5720651c384e
cesarRmrz <cesar960106@gmail.com>
2020-04-07 19:27:09 -0500

    Commit Inicial

---
+++ Assets/Scripts/SeguimientoCamara.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeguimientoCamara : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField]
+    GameObject jugadorSeguir;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(jugadorSeguir.transform.position.x,
+            jugadorSeguir.transform.position.y - 1f,
+            transform.position.z);
+    }
+}
