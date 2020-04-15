677ac5fd3ff2b6e769076a08c56fd6da748e7bbf
corona94 <51008000+corona94@users.noreply.github.com>
2020-04-15 02:00:58 -0500

    mmh

--- Assets/Scripts/Movimineto_Jugador.cs
+++ Assets/Scripts/Movimineto_Jugador.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Movimineto_Jugador : MonoBehaviour
 {
+    public Animator animator;
+
+    public bool grounded;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,5 +18,19 @@ public class Movimineto_Jugador : MonoBehaviour
     {
         Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
         transform.position = transform.position + horizontal * Time.deltaTime;
+
+       // Jump();
     }
+
+    /*void Jump()
+    {
+        Debug.Log("ENTER JUMP");
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            jumpAnimator.SetFloat("Jump", Input.GetAxis("Jump"));
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+        }
+
+
+    }*/
 }
